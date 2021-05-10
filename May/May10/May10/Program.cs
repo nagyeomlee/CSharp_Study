47f1328274c81ed76773eb9fd7592276414ee4d1
nagyeom <kb.nglee@gmail.com>
2021-05-10 18:14:37 +0900

    C#_Class

---
+++ May/May10/May10/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace May10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class_print();
+            //List_Class();
+            //List_element_add();
+            List_element_remove();
+        }
+
+        static void Class_print()
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(Math.Round(random.NextDouble(), 4)); // 랜덤 실수 발행, 소수점 4자리까지 출력
+
+        }
+
+        static void List_Class()
+        {
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+            List<int> list = new List<int>();
+
+        }
+
+        static void List_element_add()
+        {
+            List<int> list = new List<int>();
+
+            list.Add(52);
+            list.Add(573);
+            list.Add(32);
+            list.Add(64);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\titem : " + item);
+            }
+
+        }
+
+        static void List_element_remove()
+        {
+            List<int> list = new List<int>() { 11, 22, 33, 44 };
+            list.Remove(33);
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\titem : " + item);
+            }
+        }
+    }
+}
---
+++ May/May10/May10_16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace May10_16
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int result = Ma.Plus(1, 2);
+            Console.WriteLine(result);
+            int result2 = Print.Do();
+            Console.WriteLine(result2);
+        }
+    }
+
+    class Ma
+    {
+        public static int Plus(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+    class Print
+    {
+        public static int Do()
+        {
+            Console.WriteLine(Ma.Plus(3, 4));
+            return Ma.Plus(3, 4);
+        }
+    }
+}
